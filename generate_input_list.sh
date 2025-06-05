37023e049cb51617d727c125924c2bde6411b2b3
J King Chang <jc4878@gadi-login-01.gadi.nci.org.au>
2025-06-05 23:33:49 +1000

    edited launch.sh

---
+++ generate_input_list.sh
@@ -0,0 +1,30 @@
+#!/bin/bash#!/bin/bash
+
+#
+export workingdir="/path/to/workingdir" #Directory where your copy of config.sh is in
+export raw_dir="/path/to/directory_with_raw_sequences" #directory containing all of your raw fastq files, can not be in any sub-directories
+export R1_identifier="_R1.fq.gz" #the file extension of your forward sequence file, usually contains "R1"
+export R2_identifier="_R2.fq.gz" #the file extension of your reverse sequence file, usually contains "R2"
+
+
+
+##                              v1.1                                    ##
+##                      END CONFIGURATION AREA                          ##
+##----------------------------------------------------------------------##
+##----------------------------------------------------------------------##
+##  Do not edit below this line                                         ##
+##----------------------------------------------------------------------##
+
+
+if [ -d "$raw_dir" ] && [ "$(ls -A "$raw_dir")" ]; then
+    echo "$raw_dir exist and contain files, now generating inputs.csv"
+    for i in ${raw_dir}/*${R1_identifier}; do
+        filename=$(basename ${i/$R1_identifier/})
+        R1_PATH="$i"
+        R2_PATH="${raw_dir}/${filename}${R2_identifier}"
+        echo -e "${filename},${R1_PATH},${R2_PATH}"
+    done > ${workingdir}/inputs.csv
+else
+    echo "The raw_dir directory either does not exist or is empty. Stopping script"
+    exit 1
+fi
---
+++ launch.sh
@@ -0,0 +1,123 @@
+#!/bin/bash
+
+#====================================================================================#
+# General setting, please set these variables to your own values                     #
+# These are the settings that will be used by all scripts in the repository          #
+#====================================================================================#
+
+## The path to your copy of the repository
+export repository_path="/g/data/xl04/jc4878/github/skink-codes"             # do not put trailing "/" at the end
+## The path to the directory everything will be written to (your working directory, where the output will be stored)
+export workingdir="/g/data/xl04/jc4878/workingdir"
+## Email notification settings
+export email_notification="YES"                                             # either "YES" or "NO", If set to "YES", scripts in the repository will have line 5-6 changed to include your email address before submitting jobs.
+export email_address="z5205618@ad.unsw.edu.au"                              # Set to your email address if you want notifications, otherwise leave it empty.
+
+
+#==================================================================================#
+# Pipeline settings, are you using 1 of the 3 available pipelines here?             #
+# If not, set all of these to "NO" and set up the individual tool settings below   #
+#==================================================================================#
+
+export P1_trimmomatic_subread_bamcoverage="NO"                              # either "YES" or "NO", If set to "YES", the scripts will run Trimmomatic, Subread and bamCoverage in a single job.
+export P2_trimmomatic_subread="NO"                                          # either "YES" or "NO", If set to "YES", the scripts will run Trimmomatic and Subread in a single job.
+export P3_subread_bamcoverage="NO"                                          # either "YES" or "NO", If set to "YES", the scripts will run Subread and bamCoverage in a single job.
+export P4_repeatmodeler_repeatmasker="NO"                                   # either "YES" or "NO", If set to "YES", the scripts will run RepeatModeler and RepeatMasker in a single job.
+
+## Pipeline general settings - input list (needed for P1, P2, P3)
+export P_input_list="${workingdir}/input_list.csv"                          # leave this as it is unless you didn't generate the input list with generate_input_list.sh
+
+# Depending on which tools are included in your pipeline, set up the following tool-specific settings.
+## Pipeline specific settings - trimmomatic
+export P_trimming_option="CROP:95 HEADCROP:14 SLIDINGWINDOW:5:15 MINLEN:36" # an example of what this should look like
+
+## Pipeline specific settings - subread
+export P_genome="/path/to/genome.fa"                                        # ungzipped fasta file for genome is recommended, however gzipped format should also work for subread
+export P_seqtype="DNA"                                                      # either "DNA" or "RNA", RNA is usually for P2
+
+## Pipeline specific settings - bamCoverage
+export P_binsize="20000"                                                    # the bin size for bamCoverage, 20kb works well in skinks with largest chromosome at ~300Mbp
+
+## Pipeline specific settings - repeatmodeler
+
+## Pipeline specific settings - repeatmasker
+
+
+#=====================================================================================#
+# Individual tool settings, which specific tool are you running? choose "YES" or "NO" #
+# For now, only one tool can be launched (set to YES) at a time                       #
+#=====================================================================================#
+
+export trimmomatic="NO"                                                     # either "YES" or "NO", If set to "YES", the script will run Trimmomatic
+export subread="NO"                                                         # either "YES" or "NO", If set to "YES", the script will run Subread
+export bamCoverage="NO"                                                     # either "YES" or "NO", If set to "YES", the script will run bamCoverage
+export trinity="NO"                                                         # either "YES" or "NO", If set to "YES", the script will run Trinity
+export repeatmodeler="NO"                                                   # either "YES" or "NO", If set to "YES", the script will run RepeatModeler
+export repeatmasker="NO"                                                    # either "YES" or "NO", If set to "YES", the script will run RepeatMasker
+
+## Individual tool general settings - input list (needed for trimmomatic, subread)
+export T_input_list="${workingdir}/input_list.csv"                          # leave this as it is unless you didn't generate the input list with generate_input_list.sh
+
+# Depending on which tools you are launching, set up the following tool-specific settings.
+## Individual tool specific settings - trimmomatic
+export T_trimming_option="CROP:95 HEADCROP:14 SLIDINGWINDOW:5:15 MINLEN:36" # an example of what this should look like
+
+## Individual tool specific settings - subread
+export T_genome="/path/to/genome.fa"                                        # ungzipped fasta file for genome is recommended, however gzipped format should also work for subread
+export T_seqtype="DNA"                                                      # either "DNA" or "RNA"
+
+## Individual tool specific settings - bamCoverage
+export T_binsize="20000"                                                    # the bin size for bamCoverage, 20kb works well in skinks with largest chromosome at ~300Mbp
+export T_bamdir="/path/to/dir/with/bam/files"                               # the directory where the .bam files are located
+
+## Individual tool specific settings - trinity
+
+
+# Individual tool specific settings - repeatmodeler
+
+
+# Individual tool specific settings - repeatmasker
+
+
+
+
+
+
+
+#                              End of Configuration                              #
+#                           Do not edit below this line                          #
+#--------------------------------------------------------------------------------#
+
+## Adding email to scripts if email_notification is set to "YES"
+if [ "$email_notification" = "YES" ]; then
+    echo -e "[LOG] \$email_notification set to: YES, now adding email to scripts"
+    sed -i "5s|.*|#PBS -M ${email_address}|" ${repository_path}/scripts/DNA_processing/*.sh
+    sed -i "5s|.*|#PBS -M ${email_address}|" ${repository_path}/scripts/RNA_processing/*.sh
+    sed -i "6s|.*|#PBS -m ae|" ${repository_path}/scripts/DNA_processing/*.sh
+    sed -i "6s|.*|#PBS -m ae|" ${repository_path}/scripts/RNA_processing/*.sh
+elif [ "$email_notification" = "NO" ]; then
+    echo -e "[LOG] \$email_notification set to: NO, moving on"
+fi
+## Checking if only 1 pipeline is selected
+selected_pipelines=0
+[ "$P1_trimmomatic_subread_bamcoverage" = "YES" ] && selected_pipelines=$((selected_pipelines+1))
+[ "$P2_trimmomatic_subread" = "YES" ] && selected_pipelines=$((selected_pipelines+1))
+[ "$P3_subread_bamcoverage" = "YES" ] && selected_pipelines=$((selected_pipelines+1))
+
+echo -e "[LOG] Checking if you are running a pipeline or individual tools"
+if [ "$selected_pipelines" -gt 1 ]; then
+    echo "Error: Multiple pipelines set to 'YES'. Please select only one"
+    exit 1
+elif [ "$selected_pipelines" -eq 1 ]; then
+    echo "[LOG] A pipeline detected, running pipeline"
+    if [ "$P1_trimmomatic_subread_bamcoverage" == "YES" ]; then
+        echo "[LOG] Running pipeline: P1_trimmomatic_subread_bamcoverage"
+        qsub -P ${PROJECT} 
+    elif [ "$P2_trimmomatic_subread" == "YES" ]; then
+        echo "[LOG] Running pipeline: P2_trimmomatic_subread"
+    elif [ "$P3_subread_bamcoverage" == "YES" ]; then
+        echo "[LOG] Running pipeline: P3_subread_bamcoverage"
+    fi
+    exit 1
+fi
+echo "[LOG] Not running pipeline, moving on to individual tools"
