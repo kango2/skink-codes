f3553c941844fbfc80f291a2b81935c5c78930c1
J King Chang <jc4878@gadi-login-08.gadi.nci.org.au>
2025-06-07 06:19:35 +1000

    prefix variable used by default PBS it seems? changing it to species

--- scripts/DNA_processing/P_repeatmodeler.sh
+++ scripts/DNA_processing/P_repeatmodeler.sh
@@ -5,25 +5,25 @@
 
 
 
-# set -ex
+set -ex
 module use /g/data/if89/apps/modulefiles
 module load RepeatModeler/2.0.4-conda
 
 cd ${workingdir}/repeatmodeler_${directory_name}
 mkdir database
 cd database
-BuildDatabase -name ${prefix} -engine ncbi ${genome}
+BuildDatabase -name ${species} -engine ncbi ${genome}
 
-mkdir -p $TMPDIR/${prefix}_RepeatModeler
-cd $TMPDIR/${prefix}_RepeatModeler
-RepeatModeler -database ${workingdir}/repeatmodeler_${directory_name}/database/${prefix} -threads ${PBS_NCPUS} > ${workingdir}/repeatmodeler_${directory_name}/out.log
+mkdir -p $TMPDIR/${species}_RepeatModeler
+cd $TMPDIR/${species}_RepeatModeler
+RepeatModeler -database ${workingdir}/repeatmodeler_${directory_name}/database/${species} -threads ${PBS_NCPUS} > ${workingdir}/repeatmodeler_${directory_name}/out.log
 export RM_folder=$(ls | grep RM_)
 tar cf - ${RM_folder} | pigz -p ${PBS_NCPUS} > ${RM_folder}.tar.gz
 rsync ${RM_folder}.tar.gz ${workingdir}
 
 #output repeat library will be in ${workingdir}/database named:
-#${prefix}-families.fa
-#${prefix}-families.stk
+#${species}-families.fa
+#${species}-families.stk
 #the .fa file can be fed directory into repeatmasker for soft masking, or you can concatenate it with the taxon repeats library for better repeat coverage
 
 # logging package versions
